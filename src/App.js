9046757871252aa309805d576b3453d0ff1e3018
larissa <larza.ault@gmail.com>
2023-02-09 13:44:31 -0800

    Adjusted selectedBoardState to selectedBoardStateID

--- src/App.js
+++ src/App.js
@@ -1,34 +1,60 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-
 import BoardsDropDown from "./components/boards/BoardsDropDown";
 import SelectedBoard from "./components/boards/SelectedBoard";
 import NewBoardForm from "./components/boards/NewBoardForm";
-
 import axios from "axios";
 import CardList from "./components/cards/CardList";
 import ShowCard from "./components/cards/ShowCard";
 
+// ~~~~~~ Helper Functions ~~~~~~
+// Function for sending get requests
+const getAllBoards = () => {
+  return axios
+    .get(`${process.env.REACT_APP_BACKEND_URL}/boards`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.error(error.response.data.message);
+    });
+};
+// Function for sending post requests
+const registerNewBoard = (newBoard) => {
+  // create request body
+  const requestBody = {
+    ...newBoard,
+  };
+  console.log(requestBody);
+  // send post request
+  return axios
+    .post(`${process.env.REACT_APP_BACKEND_URL}/boards`, requestBody)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error.response.data.message);
+    });
+};
+
 function App() {
   // ~~~~~~ boards data ~~~~~~
   const [boardsData, setBoardsData] = useState([]);
-  // function to initialize boards data upon mounting
+  // function to make get request whenever boardsData gets modified
   useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_BACKEND_URL}/boards`)
-      .then((response) => {
-        setBoardsData(response.data);
-      })
-      .catch((error) => {
-        console.error(error.response.data.message);
-      });
+    getAllBoards().then((boards) => {
+      setBoardsData(boards);
+    });
   }, []);
-  // function to update boards data upon form submission
+  // function to update boards data state upon submission
   const updateBoardsData = (newBoard) => {
-    // duplicate boardsData
+    // make a post request to the backend
+    registerNewBoard(newBoard);
+    // change the boardsData state
     const newBoardsData = [...boardsData];
     // find next valid board id
     const nextID = Math.max(...newBoardsData.map((board) => board.id)) + 1;
+    // update the boards data state with new board
     newBoardsData.push({
       id: nextID,
       title: newBoard.title,
@@ -37,13 +63,11 @@ function App() {
     setBoardsData(newBoardsData);
   };
 
-  // selected boards message
-  const [selectedBoard, setSelectedBoard] = useState();
-  // function to update selected board message
-  const updateSelectedBoard = (selectedBoard) => {
-    setSelectedBoard(selectedBoard);
-    console.log(selectedBoard);
-    //console.log(selectedBoard[0])
+  // state to track selected board id
+  const [selectedBoardID, setSelectedBoard] = useState(0);
+  // function to update selected board state id
+  const updateSelectedBoard = (selectedBoardElementID) => {
+    setSelectedBoard(selectedBoardElementID);
   };
 
   return (
@@ -62,21 +86,24 @@ function App() {
         <header>
           <h3>Selected Board</h3>
         </header>
-        <SelectedBoard selectedBoard={selectedBoard}></SelectedBoard>
+        <SelectedBoard
+          selectedBoardID={selectedBoardID}
+          boardsData={boardsData}
+        ></SelectedBoard>
         <header>
           <h3>Create a New Board</h3>
         </header>
         <NewBoardForm updateBoardsData={updateBoardsData}></NewBoardForm>
       </section>
-      <ShowCard />
-      <CardList
+      {/* <ShowCard /> */}
+      {/* <CardList
         selectedBoard={selectedBoard}
-        //cardsData={cardsData}
-        //likeCard={likeCard}
-        //deleteCard={deleteCard}
-        //addCard={addCard}
-        //getBoardCards={getBoardCards}
-      />
+        cardsData={cardsData}
+        likeCard={likeCard}
+        deleteCard={deleteCard}
+        addCard={addCard}
+        getBoardCards={getBoardCards}
+      /> */}
       <footer>
         <h4>Made by Anna, Larissa, Melody, Supriya </h4>
       </footer>
