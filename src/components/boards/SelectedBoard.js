f534b2ec359d329d3fad9ad51be575c7e7dfb7a8
larissa <larza.ault@gmail.com>
2023-02-09 13:45:57 -0800

    Took in selectedBoardID state prop to check for selected board --> changes basic display message to board specific message

--- src/components/boards/SelectedBoard.js
+++ src/components/boards/SelectedBoard.js
@@ -1,7 +1,29 @@
 import PropTypes from "prop-types";
 
 const SelectedBoard = (props) => {
-  return <p>{props.selectedBoardMessage}</p>;
+  // state to track selected board display message
+  let message = "Select a Board from the Board List!";
+  //if a board is selected
+  if (props.selectedBoardID !== 0) {
+    // find the corresponding board to selectedBoardID
+    const selectedBoard = props.boardsData.filter(
+      (board) => board.id === props.selectedBoardID
+    );
+    message = `${selectedBoard[0].title} - ${selectedBoard[0].creator}`;
+  }
+  // update the message state
+  return <p>{message}</p>;
+};
+
+SelectedBoard.propTypes = {
+  boardsData: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      creator: PropTypes.string.isRequired,
+    })
+  ),
+  selectedBoardID: PropTypes.number.isRequired,
 };
 
 export default SelectedBoard;
